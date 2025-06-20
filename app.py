a3770f7e47324be65fb87934a48e0a7bc4cd0a9f
Priya <asthenua@example.com>
2025-06-20 17:57:03 +0530

    Fix Twilio MessagingResponse import

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 import requests
 
-from twilio.twiml.messaging_response
-import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
